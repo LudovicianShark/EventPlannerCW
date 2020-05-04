c1d7aee35c0693a7a8af0fa230b9580c8d583a1c
Brad Thomson <bradthomson1997@gmail.com>
2020-05-04 10:15:04 +0100

    Version 1.0.2

--- mern_app/server.js
+++ mern_app/server.js
@@ -23,15 +23,15 @@ mongoose
 //Use routes
 app.use('/api/items', items);
 
-// // Server static assests if in prpduction
-// if(process.env.NODE_ENV  === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
+// Server static assests if in prpduction
+if(process.env.NODE_ENV  === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 const port = process.env.PORT || 5000;
