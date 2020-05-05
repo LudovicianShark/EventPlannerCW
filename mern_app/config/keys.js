5ce27261dd164b617b8188b10bb11b8c87840ad9
Brad Thomson <bradthomson1997@gmail.com>
2020-05-05 23:13:46 +0100

    Secon Push

--- mern_app/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = { 
-    mongoURI : 'mongodb+srv://brad:brad1234@cwdb-s89dl.mongodb.net/test?retryWrites=true&w=majority'
-}
--- mern_app/server.js
+++ mern_app/server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const BodyParser = require('body-parser');
 const path = require('path');
-
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
 
 //Bodyparser Midlleware
-app.use(BodyParser.json());
+app.use(express.json());
 
 // DB configuration
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 //Connect to mongo
 mongoose
-    .connect(db)
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
     .then(() => console.log('MongoDB Successfully Connect...'))
     .catch(err => console.log(err));
 
 //Use routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Server static assests if in prpduction
 if(process.env.NODE_ENV  === 'production') {
